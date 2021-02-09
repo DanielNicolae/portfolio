aa38f2304dd917ea0a6f531d90907667b8b6eefa
Daniel Nicolae <DeadlyPoet@yandex.com>
2021-02-09 12:34:58 +0200

    Added the 2 new projects: Etch-a-sketch and Calculator.

--- src/components/card/Cards.js
+++ src/components/card/Cards.js
@@ -20,7 +20,16 @@ const Cards = () => {
                 src="../videos/polygonalnumbers.gif"
                 name="Polygonal numbers"
                 description="This is a project made in React. You can calculate the first 100 poligonal numbers with a certain number of vertices." />
-                
+            <Card 
+                path="https://danielnicolae.github.io/Etch-a-Sketch/"
+                src="../videos/etch-a-sketch.gif"
+                name="Etch-a-sketch"
+                description="It's a cancas made in plain JavaScript. You draw by dragging the mouse over the canvas; you can choose the size of the canvas and pick any color." />
+            <Card 
+                path="https://danielnicolae.github.io/Calculator/"
+                src="../videos/calculator.gif"
+                name="Calculator"
+                description="This is a calculator made in vanila JavaScript; it has keyboard functionality." />    
         </div>
     );
 }
