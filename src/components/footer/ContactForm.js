ec1ae0ce7b227854ee24bcfafbd4899e5c5ccba5
Daniel Nicolae <DeadlyPoet@yandex.com>
2021-03-19 23:22:13 +0200

    I fixed the labels for the email fields

--- src/components/footer/ContactForm.js
+++ src/components/footer/ContactForm.js
@@ -4,9 +4,9 @@ import {useForm} from 'react-hook-form';
 import {db} from '../firebase';
 
 const ContactForm = () => {
-    let [email, setEmail] = useState("");
-    let [subject, setSubject] = useState("");
-    let [message, setMessage] = useState("");
+    let [email, setEmail] = useState('');
+    let [subject, setSubject] = useState('');
+    let [message, setMessage] = useState('');
 
     let {register, handleSubmit} = useForm();
 
@@ -31,12 +31,12 @@ const ContactForm = () => {
 
     return (
         <form className='contactForm' onSubmit={handleSubmit}>
-            <label for="email">*Your email</label>
-            <input className='input' type='email' placeholder='*Your email' name='email' value={email} onChange={(e) => setEmail(e.target.value)} ref={register({required: true})} />
-            <label for="subject">*Subject</label>
-            <input className='input' type='text' placeholder='*Subject' name='subject' value={subject} onChange={(e) => setSubject(e.target.value)} ref={register({required: true})} />
-            <label for="message">*Message</label>
-            <textarea className='input messageArea' type='text' placeholder='*Message...' name='message' value = {message} onChange={(e) => setMessage(e.target.value)} ref={register({required: true})} />
+            <label for='email'>*Your email</label>
+            <input id='email' className='input' type='email' placeholder='*Your email' name='email' value={email} onChange={(e) => setEmail(e.target.value)} ref={register({required: true})} />
+            <label for='subject'>*Subject</label>
+            <input id='subject' className='input' type='text' placeholder='*Subject' name='subject' value={subject} onChange={(e) => setSubject(e.target.value)} ref={register({required: true})} />
+            <label for='message'>*Message</label>
+            <textarea id='message' className='input messageArea' type='text' placeholder='*Message...' name='message' value = {message} onChange={(e) => setMessage(e.target.value)} ref={register({required: true})} />
             <p>* The field is required</p>
             <button className='submitButton' type='submit'>
                 <span className='submitText'>SUBMIT</span>
