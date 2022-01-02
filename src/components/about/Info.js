7d96662b59cb4956bb8837f761df54397bfe640e
DanielNicolae <deadlypoet@protonmail.com>
2022-01-02 22:19:14 +0200

    I changed the class name from 'container' to 'InfoContainer' in Info.js.

--- src/components/about/Info.js
+++ src/components/about/Info.js
@@ -5,18 +5,19 @@ function Info() {
     return (
         <>
             <h1>Achivements</h1>
-            <div className="container">
-
-                <ul>
-                    <a className="info" href='https://courses.edx.org/certificates/104c73449a9340108ea665d055cd8150' rel="noreferrer" target="_blank">
-                        CS50's Web Programming with Python and JavaScript
-                </a>
-                    <p className="info">CS50 Introduction in computer science</p>
-                    <p className="info">I graduated a manual QA tester course at Wantsome.</p>
-                    <a className="info" href="https://play.google.com/store/apps/developer?id=Don%C8%9Bu+Daniel+Nicolae" rel="noreferrer" target="_blank">
-                        I created video games for Android in Unity.
-                </a>
-                </ul>
+            <div className="InfoContainer">
+                <div>
+                    <ul>
+                        <a className="info" href='https://courses.edx.org/certificates/104c73449a9340108ea665d055cd8150' rel="noreferrer" target="_blank">
+                            CS50's Web Programming with Python and JavaScript
+                        </a>
+                        <p className="info">CS50 Introduction in computer science</p>
+                        <p className="info">I graduated a manual QA tester course at Wantsome.</p>
+                        <a className="info" href="https://play.google.com/store/apps/developer?id=Don%C8%9Bu+Daniel+Nicolae" rel="noreferrer" target="_blank">
+                            I created video games for Android in Unity.
+                        </a>
+                    </ul>
+                </div>
                 <a href='https://courses.edx.org/certificates/104c73449a9340108ea665d055cd8150' rel="noreferrer" target="_blank">
                     <img src='../images/CS50W.PNG' alt='CS50W certificate'></img>
                 </a>
--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -3,19 +3,19 @@ import './Card.css';
 
 const Card = (props) => {
     return (
-        
+
         <a className="box" href={props.path} rel="noreferrer" target="_blank">
             <div className="videoBox">
-                <img src={props.src} width='100%' alt="project" />
+                <img src={props.src} width='100%' alt={props.name} />
             </div>
             <div className="textBox">
                 <div className="text">
                     <h3>{props.name}</h3>
                     <p>{props.description}</p>
-                </div> 
-            </div>           
+                </div>
+            </div>
         </a>
-        
+
     );
 }
 
