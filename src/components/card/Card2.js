04fe980e74133921942f86838ff45b33fd430206
DanielNicolae <deadlypoet@protonmail.com>
2022-01-04 03:33:35 +0200

    I replaced the gif with a mp4 video for the projects: ducks vs trucks and to-do list.

---
+++ src/components/card/Card2.js
@@ -0,0 +1,17 @@
+import './Card.css';
+
+export default function Card2(props) {
+  return (
+    <a className="box" href={props.path} rel="noreferrer" target="_blank">
+      <video className="videoBox" autoPlay loop muted >
+        <source src={props.src} type="video/mp4" />
+      </video>
+      <div className="textBox">
+        <div className="text">
+          <h3>{props.name}</h3>
+          <p>{props.description}</p>
+        </div>
+      </div>
+    </a>
+  );
+}
--- src/components/card/Cards.js
+++ src/components/card/Cards.js
@@ -1,10 +1,35 @@
 import React from 'react';
 import Card from './Card';
 import './Cards.css';
+import Card2 from './Card2';
 
 const Cards = () => {
     return (
         <div className="container">
+            <Card2
+                path="https://ducks-vs-trucks.netlify.app/"
+                src="./videos/ducks-vs-trucks.mp4"
+                name="Ducks vs trucks"
+                description="This is a project made in React. 
+                It's a simple card game. You play with the computer. 
+                Each card has a life and an attack power.
+                As you click on one of your cards, the computer selects a random card; the attack power of each card is subtracted from the life of the opposite card.
+                The sum of the lives of each card determines the winner at the end of the final round." />
+            <Card2
+                path="https://danielnicolae.github.io/todo-list/"
+                src="./videos/to-do-list.mp4"
+                name="To-do list"
+                description="This is part of the JavaScript course from theodinproject.com" />
+            <Card
+                path="https://danielnicolae.github.io/Calculator/"
+                src="../videos/calculator.gif"
+                name="Calculator"
+                description="This is a calculator made in vanilla JavaScript; it has keyboard functionality." />
+            <Card
+                path="https://danielnicolae.github.io/Etch-a-Sketch/"
+                src="../videos/etch-a-sketch.gif"
+                name="Etch-a-sketch"
+                description="It's a cancas made in plain JavaScript. You draw by dragging the mouse over the canvas; you can choose the size of the canvas and pick any color." />
             <Card
                 path="https://deadly-poet.herokuapp.com/"
                 src="../videos/myworks.gif"
@@ -20,21 +45,6 @@ const Cards = () => {
                 src="../videos/polygonalnumbers.gif"
                 name="Polygonal numbers"
                 description="This is a project made in React. You can calculate the first 100 poligonal numbers with a certain number of vertices." />
-            <Card
-                path="https://danielnicolae.github.io/Etch-a-Sketch/"
-                src="../videos/etch-a-sketch.gif"
-                name="Etch-a-sketch"
-                description="It's a cancas made in plain JavaScript. You draw by dragging the mouse over the canvas; you can choose the size of the canvas and pick any color." />
-            <Card
-                path="https://danielnicolae.github.io/Calculator/"
-                src="../videos/calculator.gif"
-                name="Calculator"
-                description="This is a calculator made in vanila JavaScript; it has keyboard functionality." />
-            <Card
-                path="https://danielnicolae.github.io/todo-list/"
-                src="../videos/todolist.gif"
-                name="Todo list"
-                description="This is part of the JavaScript path on theodinproject.com" />
         </div>
     );
 }
