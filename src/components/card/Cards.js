2cea1331fdf30f21e5d22e4821a70b63cd662f18
DanielNicolae <deadlypoet@protonmail.com>
2022-01-07 00:35:27 +0200

    I changed the gif in a mp4 for the projects freegames and polygonal-numbers.

--- src/components/card/Cards.js
+++ src/components/card/Cards.js
@@ -30,14 +30,14 @@ const Cards = () => {
                 src="../videos/myworks.gif"
                 name="My works"
                 description="This is a project made in Django. I used webscrapping with python (beautiful soup) to gather data from the web." />
-            <Card
+            <Card2
                 path="https://free-steam-epic-games.herokuapp.com/"
-                src="../videos/freegames.gif"
+                src="../videos/freegames.mp4"
                 name="Free video games"
                 description="This is a project made in Django. I used webscrapping with python (beautiful soup) to gather images from the web." />
-            <Card
+            <Card2
                 path="https://polygonal-numbers-27298a.netlify.app/"
-                src="../videos/polygonalnumbers.gif"
+                src="../videos/polygonal-numbers.mp4"
                 name="Polygonal numbers"
                 description="This is a project made in React. You can calculate the first 100 poligonal numbers with a certain number of vertices." />
         </div>
